40b12a8cd25b2e8a5ce52cf53a77293f686cd583
Paulo Ramos <pauloramos64@gmail.com>
2018-12-06 14:26:02 -0800

    add babel-eslint package for non-CRA apps

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 YELLOW='\033[1;33m'
 GREEN='\033[1;32m'
 LCYAN='\033[1;36m'
@@ -9,7 +11,7 @@ echo -e "1/5 ${LCYAN}Local ESLint & Prettier Installation... ${NC}"
 npm install -D eslint@5.6.0 prettier
 
 echo -e "2/5 ${YELLOW}Airbnb Configuration Installation... ${NC}"
-npm install -D eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react
+npm install -D eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react babel-eslint
 
 echo -e "3/5 ${LCYAN}Disabling ESLint Formatting... ${NC}"
 npm install -D eslint-config-prettier eslint-plugin-prettier eslint-plugin-flowtype@2.50.3 eslint-config-react-app
